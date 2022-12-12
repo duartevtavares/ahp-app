68b947e509415e07440c08973baf8e9eb24e0935
Duarte <duartetavares18@gmail.com>
2022-12-12 01:02:17 +0000

    chore: matrix more or less organized

--- ahpApp/src/app/components/decision/new-decision/new-decision.component.ts
+++ ahpApp/src/app/components/decision/new-decision/new-decision.component.ts
@@ -18,6 +18,8 @@ export class NewDecisionComponent implements OnInit {
   criteriaComparisonsValues: any[] = [];
   alternativeComparisonsValues: any[] = [];
 
+  matrix: any[] = [];
+
   initialValue = 5;
 
   constructor(public specsService: DecisionSpecificationsService) {}
@@ -33,6 +35,7 @@ export class NewDecisionComponent implements OnInit {
     );
 
     this.createArraysInitialValues();
+    this.createEmptyMatrix();
   }
 
   createDecisionComparisonColumns(namesToBeCompared: [], step: string) {
@@ -74,9 +77,90 @@ export class NewDecisionComponent implements OnInit {
 
   changeCriteriaValue(event: any, index: number) {
     this.criteriaComparisonsValues[index] = event.value;
+    // this.changeInputValuesToMatrixValues(this.criteriaComparisonsValues);
+
+    //this.changeInputValuesToMatrixValues(this.criteriaComparisonsValues);
+    this.createEmptyMatrix();
+    //this.createMatrix(newValuesArray);
   }
 
   changeAlternativeValue(event: any, index: number, otherIndex: number) {
     this.alternativeComparisonsValues[index][otherIndex] = event.value;
+  }
+
+  createEmptyMatrix() {
+    for (let i = 0; i < this.decisionCriteria.length; i++) {
+      this.matrix.push([]);
+    }
+    for (let i = 0; i < this.decisionCriteria.length; i++) {
+      for (let j = 0; j < this.decisionCriteria.length; j++) {
+        this.matrix[i][j] = 0;
+      }
+    }
+  }
+
+  createMatrix(vector: number[]) {
+    let k = 0;
+    for (let i = 0; i < this.decisionCriteria.length; i++) {
+      for (let j = 1 + i; j < this.decisionCriteria.length; j++) {
+        this.matrix[i][j] = vector[k];
+        k++;
+      }
+    }
+
+    k = 0;
+    for (let i = 0; i < this.decisionCriteria.length; i++) {
+      for (let j = 1 + i; j < this.decisionCriteria.length; j++) {
+        let stringVal = vector[k].toString();
+        this.matrix[j][i] = '1/' + stringVal;
+        k++;
+      }
+    }
+
+    for (let i = 0; i < this.decisionCriteria.length; i++) {
+      for (let j = 0; j < this.decisionCriteria.length; j++) {
+        if (i === j) {
+          this.matrix[i][j] = 1;
+        }
+      }
+    }
+    console.log(this.matrix);
+  }
+
+  changeInputValuesToMatrixValues(valuesArray: number[]) {
+    let newValuesArray = [];
+    console.log(valuesArray.length);
+    for (let i = 0; i < valuesArray.length; i++) {
+      switch (valuesArray[i]) {
+        case 1:
+          newValuesArray[i] = 1 / 9;
+          break;
+        case 2:
+          newValuesArray[i] = 1 / 7;
+          break;
+        case 3:
+          newValuesArray[i] = 1 / 5;
+          break;
+        case 4:
+          newValuesArray[i] = 1 / 3;
+          break;
+        case 5:
+          newValuesArray[i] = 1;
+          break;
+        case 6:
+          newValuesArray[i] = 3;
+          break;
+        case 7:
+          newValuesArray[i] = 5;
+          break;
+        case 8:
+          newValuesArray[i] = 7;
+          break;
+        case 9:
+          newValuesArray[i] = 9;
+          break;
+      }
+    }
+    console.log(newValuesArray);
   }
 }
